513f715a03cd168384feee59ad185bbf726687d0
Zhou Yuhang <zyuhang255@gmail.com>
2025-01-14 06:33:51 +0800

    codecrafters submit [skip ci]

--- src/Server.cpp
+++ src/Server.cpp
@@ -185,11 +185,8 @@ void handleClient(int client_fd, const std::string &dir,
           is.read(header, 9);
           std::unordered_map<std::string, std::string> keyValue;
 
-          int maxTimes = 5;
           // process segments
           while (true) {
-            if (!maxTimes--)
-              break;
             uint8_t opcode = readByte(is);
             std::cout << "\nOpcode: " << std::to_string(opcode) << "\n";
             // metadata section
