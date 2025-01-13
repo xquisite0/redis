a7cf459c572173daded9fc230880b9e01a42dc0b
Zhou Yuhang <zyuhang255@gmail.com>
2025-01-13 20:03:53 +0800

    codecrafters submit [skip ci]

--- src/Server.cpp
+++ src/Server.cpp
@@ -182,7 +182,7 @@ void handleClient(int client_fd, const std::string &dir,
           is.read(header, 9);
           std::unordered_map<std::string, std::string> keyValue;
 
-          int maxTimes = 20;
+          int maxTimes = 5;
           // process segments
           while (true) {
             if (!maxTimes--)
