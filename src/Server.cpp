4dae113ccdf462309b14581fdf521d6fd1314d2d
Zhou Yuhang <zyuhang255@gmail.com>
2025-01-13 19:51:22 +0800

    codecrafters submit [skip ci]

--- src/Server.cpp
+++ src/Server.cpp
@@ -34,7 +34,7 @@ int readLength(std::ifstream &is) {
   std::cout << "\n First Byte: " << std::to_string(firstByte) << "\n";
 
   uint8_t flag = firstByte >> 6;
-  uint8_t value = firstByte | 0x3F;
+  uint8_t value = firstByte & 0x3F;
   std::cout << "\n Flag & Value: " << std::to_string(flag) << " "
             << std::to_string(value) << "\n";
 
