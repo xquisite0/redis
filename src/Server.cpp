1af75129d8542ee614a0d5ecef877c6c3d66928c
Zhou Yuhang <zyuhang255@gmail.com>
2025-01-15 12:19:44 +0800

    codecrafters submit [skip ci]

--- src/Server.cpp
+++ src/Server.cpp
@@ -85,9 +85,10 @@ void parseRDB(
 
   // skip header section
   char header[9];
-  // std::cout << "\n\nReading Header...\n\n";
+  std::cout << "\n\nReading Header...\n\n";
   is.read(header, 9);
-  // std::cout << "\n\nRead header!\n\n";
+  std::cout << "\n\nRead header!\n\n";
+  std::cout << header << "\n";
   // std::unordered_map<std::string, std::string> keyValue;
 
   bool expirySet = false;
