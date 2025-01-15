c0ee70e5df511aa07b95f692f5f22aeeb40008f7
Zhou Yuhang <zyuhang255@gmail.com>
2025-01-15 12:02:45 +0800

    codecrafters submit [skip ci]

--- src/Server.cpp
+++ src/Server.cpp
@@ -145,7 +145,7 @@ void parseRDB(std::unordered_map<std::string, std::string> &keyValue,
       unsigned long time = 0;
       for (int i = 0; i < 8; i++) {
         uint8_t byte = readByte(is);
-        std::cout << std::hex << byte << std::endl;
+        std::cout << byte << " " << std::endl;
         time <<= 8;
         time |= byte;
       }
