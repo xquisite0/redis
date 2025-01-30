da420eaef604ce2a3882c3ce033177481ee52225
Zhou Yuhang <zyuhang255@gmail.com>
2025-01-30 14:21:39 +0800

    codecrafters submit [skip ci]

--- src/Server.cpp
+++ src/Server.cpp
@@ -567,6 +567,7 @@ int main(int argc, char **argv) {
     send(clientSocket, message.c_str(), message.size(), 0);
 
     response = receiveResponse(clientSocket);
+    response = receiveResponse(clientSocket);
     std::cout << "\n\n Hi there! Here's the RDB response (checking if replconf "
                  "cmmnd is here): "
               << response << std::endl;
