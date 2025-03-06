37d5cda665bcbe37695b1f8dfdac650a81785da0
Zhou Yuhang <zyuhang255@gmail.com>
2025-03-06 12:59:18 +0800

    codecrafters submit [skip ci]

--- src/Server.cpp
+++ src/Server.cpp
@@ -509,10 +509,9 @@ void handleClient(int client_fd, const std::string &dir,
 
               ProtocolParser parser(fd);
               RedisMessage offsetMessage = parser.parse();
-              // std::cout << "\nFinished obtaining the message with the
-              // offset "
-              //              "of replica socket number "
-              //           << fd << "\n";
+              std::cout << "\nFinished obtaining the message with the offset "
+                           "of replica socket number "
+                        << curReplica << "\n";
               int offset = stoi(offsetMessage.elements[2].value);
               std::cout << "\nReplica socket number " << fd
                         << " gave the following offset value: "
